dd2def47094f05dac42645a6ec90743c34352c85
Daniel Ben-Or <danielbenor@yahoo.com>
2024-08-16 13:18:42 +0300

    got working postgresql db setup, set for no auth for now, will update for security closer to end of task

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/gorilla/mux v1.8.1
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
--- main.go
+++ main.go
@@ -7,14 +7,24 @@ import (
 	"golangphonebook/internal"
 	"golangphonebook/pkg/contacts"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 
 	// Set up PostgreSQL connection
-	connStr := "user=username password=password dbname=mydb sslmode=disable"
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		internal.Logger.Error(fmt.Sprintf("DB connection init failed, shutting down: %s", err))
